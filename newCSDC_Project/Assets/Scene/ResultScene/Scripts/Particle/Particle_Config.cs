04d89be1545363f5930931bcbb6e04807ceb3663
matsuda <yuzi1112m@gmail.com>
2018-12-11 15:41:21 +0900

    リザルトシーンの花吹雪と順位取得後のアニメーションを実装

---
+++ newCSDC_Project/Assets/Scene/ResultScene/Scripts/Particle/Particle_Config.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Particle_Config : MonoBehaviour
+{
+    [SerializeField]
+    private float Start_Particle;   // パーティクルの開始時間
+
+    private ParticleSystem particle;
+    ScoreManager manager;
+
+    // Use this for initialization
+    void Start()
+    {
+        particle = this.GetComponent<ParticleSystem>();
+        manager = GameObject.Find("Score").GetComponent<ScoreManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ランキング関数
+        Ranking();
+    }
+
+    void Ranking()
+    {
+        // Player１が１位の場合
+        if (manager.score[0] >= manager.score[1] && manager.score[0] >= manager.score[2] && manager.score[0] >= manager.score[3])
+        {
+            // 花吹雪がPlayer1の真上に来る
+            Vector3 tmp = this.gameObject.transform.position;
+            this.gameObject.transform.position = new Vector3(-2, tmp.y, tmp.z);
+        }
+
+        // Player２が１位の場合
+        if (manager.score[1] >= manager.score[0] && manager.score[1] >= manager.score[2] && manager.score[1] >= manager.score[3])
+        {
+            // 花吹雪がPlayer2の真上に来る
+            Vector3 tmp = this.gameObject.transform.position;
+            this.gameObject.transform.position = new Vector3(-5, tmp.y, tmp.z);
+        }
+
+        // Player３が１位の場合
+        if (manager.score[2] >= manager.score[0] && manager.score[2] >= manager.score[1] && manager.score[2] >= manager.score[3])
+        {
+            // 花吹雪がPlayer3の真上に来る
+            Vector3 tmp = this.gameObject.transform.position;
+            this.gameObject.transform.position = new Vector3(0.75f, tmp.y, tmp.z);
+        }
+
+        // Player４が１位の場合
+        if (manager.score[3] >= manager.score[0] && manager.score[3] >= manager.score[1] && manager.score[3] >= manager.score[2])
+        {
+            // 花吹雪がPlayer4の真上に来る
+            Vector3 tmp = this.gameObject.transform.position;
+            this.gameObject.transform.position = new Vector3(2, tmp.y, tmp.z);
+        }
+    }
+}
---
+++ newCSDC_Project/Assets/Scene/ResultScene/Scripts/Ranking/Player1_Ranking.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+// 制作者：松田
+public class Player1_Ranking : MonoBehaviour
+{
+    // Animator コンポーネント
+    private Animator animator;
+
+    // 勝利フラグ（現在は仮）
+    private const string win_flg = "pullFlag";
+
+    // 敗北フラグ（現在は仮）
+    private const string lose_flg = "IzimerareFlg";
+
+    // 待機フラグ
+    //    private const string setting_flg = "";
+
+    [SerializeField]
+    private float StartTime;                // 何秒後に開始
+
+    ScoreManager manager;
+
+    // Use this for initialization
+    void Start ()
+    {
+        manager = GameObject.Find("Score").GetComponent<ScoreManager>();
+
+        this.animator = GetComponent<Animator>();
+//        this.animator.SetBool(setting_flg, true);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // 時間経過処理
+        StartTime -= Time.deltaTime;
+        if (StartTime <= 0)
+        {
+            Ranking();
+        }
+	}
+
+    void Ranking()
+    {
+        // Player１が１位の場合
+        if(manager.score[0] >= manager.score[1] && manager.score[0] >= manager.score[2] && manager.score[0] >= manager.score[3])
+        {
+//            this.animator.SetBool(setting_flg, false);
+            // 勝利モーションに入る
+            this.animator.SetBool(win_flg, true);
+        }
+        // Player１が１位ではない場合
+        else
+        {
+//            this.animator.SetBool(setting_flg, false);
+            // 敗北モーションに入る
+            this.animator.SetBool(lose_flg, true);
+        }
+    }
+}
